a7dbe8ddef3e497bd779af8ce2a5729784dc2df3
xyh123321 <376564838@qq.com>
2020-08-27 15:28:42 +0800

    format method calcAmountOfType

--- src/statement.js
+++ src/statement.js
@@ -1,4 +1,4 @@
-function statement (invoice, plays) {
+function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
@@ -7,6 +7,7 @@ function statement (invoice, plays) {
     currency: 'USD',
     minimumFractionDigits: 2,
   }).format;
+
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = calcAmountOfType(play, 0, perf);
@@ -22,27 +23,27 @@ function statement (invoice, plays) {
   result += `You earned ${volumeCredits} credits \n`;
   return result;
 
+}
 
-  function calcAmountOfType(play, thisAmount, perf) {
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
-    return thisAmount;
+function calcAmountOfType(play, thisAmount, perf) {
+  switch (play.type) {
+    case 'tragedy':
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case 'comedy':
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
   }
+  return thisAmount;
 }
 
 module.exports = {
