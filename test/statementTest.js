8bc0741ca01476f7b41e4ad460cd930fa62de8c5
xyh123321 <376564838@qq.com>
2020-08-27 15:09:50 +0800

    add test case

--- test/statementTest.js
+++ test/statementTest.js
@@ -19,16 +19,90 @@ You earned 0 credits \n`
 )
 })
 
-//test('Sample test', t => {
-//  //given
-//  const invoice = {};
-//  const plays = [];
-//
-//  const result = statement(invoice, plays);
-//
-//  t.is(result, '');
-//});
+test('has perfomances and palys', t => {
+    let invoice = {
+        'customer': 'jayden',
+        'performances': [
+             {
+                  'playID': 'hamlet',
+                  'audience': 55,
+                }
+        ]
+    }
+    let plays = {
+         'hamlet': {
+            'name': 'Hamlet',
+            'type': 'tragedy',
+          }
+    }
+    let res = statement(invoice, plays);
+    t.is(res,`Statement for jayden
+ Hamlet: $650.00 (55 seats)
+Amount owed is $650.00
+You earned 25 credits \n`
+)
+})
 
+test('hava perfomances and muti palys', t => {
+    let invoice = {
+        'customer': 'jayden',
+        'performances': [
+             {
+                  'playID': 'hamlet',
+                  'audience': 55,
+                }
+        ]
+    }
+    let plays = {
+         'hamlet': {
+            'name': 'Hamlet',
+            'type': 'tragedy',
+          },
+          'as-like': {
+             'name': 'As You Like It',
+             'type': 'comedy',
+          }
+    }
+    let res = statement(invoice, plays);
+    t.is(res,`Statement for jayden
+ Hamlet: $650.00 (55 seats)
+Amount owed is $650.00
+You earned 25 credits \n`
+)
+})
+
+test('hava muti perfomances and single palys', t => {
+    let invoice = {
+        'customer': 'jayden',
+        'performances': [
+             {
+                   'playID': 'hamlet',
+                   'audience': 30,
+             },
+             {
+                   'playID': 'as-like',
+                   'audience': 35,
+             },
+        ]
+    }
+    let plays = {
+         'hamlet': {
+            'name': 'Hamlet',
+            'type': 'tragedy',
+          },
+          'as-like': {
+            'name': 'As You Like It',
+            'type': 'comedy',
+          },
+    }
+    let res = statement(invoice, plays);
+    t.is(res,`Statement for jayden
+ Hamlet: $400.00 (30 seats)
+ As You Like It: $580.00 (35 seats)
+Amount owed is $980.00
+You earned 12 credits \n`
+)
+})
 
 const invoice = {
   'customer': 'BigCo',
